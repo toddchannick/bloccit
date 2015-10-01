5634dc416ab5ba1a44bf1f4d014f98aa9c417306
Todd Channick <toc5012@gmail.com>
2015-10-01 19:17:53 -0400

    Checkpoint 33 Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
 include RandomData
 
- # Create Posts
  50.times do
- # #1
    Post.create!(
- # #2
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
 
- # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
+
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
+
+ puts "#{Post.count} posts before unique post"
+ Post.find_or_create_by(title: "Checkpoint 33 Title", body: "Checkpoint 33 Body")
+ puts "#{Post.count} posts after unique post"
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
