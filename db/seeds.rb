9b8e4992868477d65614c4f99e3cc89b756571c0
Todd Channick <toc5012@gmail.com>
2015-10-14 13:54:55 -0400

    Assignment 35 Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,14 @@ include RandomData
    )
  end
 
+ 50.times do
+   Advertisement.create!(
+     title:  RandomData.random_sentence,
+     copy:   RandomData.random_paragraph,
+     price: rand(1000)
+   )
+ end
+
  puts "#{Post.count} posts before unique post"
  Post.find_or_create_by(title: "Checkpoint 33 Title", body: "Checkpoint 33 Body")
  puts "#{Post.count} posts after unique post"
