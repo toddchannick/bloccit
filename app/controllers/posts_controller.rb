6997a2149288d63023eb63a38c48398a71548f11
Todd Channick <toc5012@gmail.com>
2015-10-14 14:44:59 -0400

    checkpoint-36-complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @posts.each_with_index do |post,index|
-      if index % 5 == 0
-        post.title = "SPAM"
-      end
-    end
   end
 
   def show
@@ -30,7 +25,34 @@ class PostsController < ApplicationController
      end
    end
 
+   def edit
+     @post = Post.find(params[:id])
+   end
 
-  def edit
-  end
+   def update
+     @post = Post.find(params[:id])
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+
+   def destroy
+     @post = Post.find(params[:id])
+
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash[:error] = "There was an error deleting the post."
+       render :show
+     end
+   end
+
 end
