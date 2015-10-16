efa1de43647fa756afa66ca051d4799c8000a751
Todd Channick <toc5012@gmail.com>
2015-10-16 18:49:59 -0400

    Checkpoint Complete - Authentication, Signing Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+
+  # if any errors are found, this will set the div/div class around each element accordingly
   def form_group_tag(errors, &block)
      if errors.any?
        content_tag :div, capture(&block), class: 'form-group has-error'
