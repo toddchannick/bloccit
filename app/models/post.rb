66ba12d633c1ba559c44c17e73e401d2b47ae110
Todd Channick <toc5012@gmail.com>
2015-10-22 23:02:29 -0400

    Assignment Complete - Added Self Upvote Feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  after_create :create_vote
+
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
@@ -6,7 +8,7 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes, dependent: :destroy
 
-  #this will order all posts by their 'created_at' date in descending order (recent first)
+
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -27,9 +29,15 @@ class Post < ActiveRecord::Base
   end
 
   def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
-     new_rank = points + age_in_days
-     update_attribute(:rank, new_rank)
-   end
+   age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+   new_rank = points + age_in_days
+   update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 
 end
