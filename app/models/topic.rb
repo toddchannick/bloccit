5d703d82e577355efacc7e36b8836778ea9849dc
Todd Channick <toc5012@gmail.com>
2015-10-28 13:58:08 -0400

    Checkpoint Complete - Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,8 @@ class Topic < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes
 
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
 end
