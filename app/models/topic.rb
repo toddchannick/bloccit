5c2c83934c588339df1a4853086b0a1c8755c471
Todd Channick <toc5012@gmail.com>
2015-10-28 14:06:56 -0400

    Assignment Complete - Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,7 +4,9 @@ class Topic < ActiveRecord::Base
   has_many :labels, through: :labelings
   has_many :votes
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :publicly_viewable -> { where(public: true) }
+  scope :privately_viewable -> { where(public: false) }
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
